96aebbf7a4c0d3b85c03bc05197638f5daac0c3c
SaintMiner <dinden290@gmail.com>
2022-11-19 00:20:45 +0200

    Improved logger

--- modules/logger.js
+++ modules/logger.js
@@ -5,38 +5,38 @@ const { client } = require('./client');
 /* Message delete */
 client.on(Events.MessageDelete, message => {
   /* WIP */
-  if (message.guild.id != "240492317679550465") return;
+  if (message.guild.id != process.env.LOGGING_SERVER) return;
   if (message.author.bot || message.author.system) return;
 
   const deletedContent = codeBlock('diff', `-${message.content}`);
-  message.guild.channels.cache.get("497477596091514882")
-    .send(`${bold(message.author.tag)} (ID: ${message.author.id}) deleted message (ID: ${message.id}): ${deletedContent}`);
+  message.guild.channels.cache.get(process.env.LOGGING_CHANNEL)
+    .send(`${bold(message.author.tag)} (ID: ${message.author.id}) deleted message (ID: ${message.id}) in ${bold(message.channel.name)} (ID: ${message.channelId}) ${deletedContent}`);
 });
 
 /* Message update */
 client.on(Events.MessageUpdate, (oldMessage, newMessage) => {
   /* WIP */
-  if (oldMessage.guild.id != "240492317679550465") return;
+  if (oldMessage.guild.id != process.env.LOGGING_SERVER) return;
   if (oldMessage.author.bot || oldMessage.author.system) return;
 
   const updatedContent = codeBlock('diff', [`-${oldMessage.content}`,`+${newMessage.content}`].join('\n'));
-  oldMessage.guild.channels.cache.get("497477596091514882")
-    .send(`${bold(newMessage.author.tag)} (ID: ${newMessage.author.id}) updated message (ID: ${newMessage.id}): ${updatedContent}`);
+  oldMessage.guild.channels.cache.get(process.env.LOGGING_CHANNEL)
+    .send(`${bold(newMessage.author.tag)} (ID: ${newMessage.author.id}) updated message (ID: ${newMessage.id}) in ${bold(oldMessage.channel.name)} channel (ID: ${oldMessage.channelId}) ${updatedContent}`);
 });
 
 /* Member join listening */
 client.on(Events.GuildMemberAdd, member => {
   /* WIP */
-  if (member.guild.id != "240492317679550465") return;
+  if (member.guild.id != process.env.LOGGING_SERVER) return;
 
-  member.guild.channels.cache.get("497477596091514882")
+  member.guild.channels.cache.get(process.env.LOGGING_CHANNEL)
     .send(`${bold(member.user.tag)} (ID: ${member.id}) just joined to server.`);
 });
 
 /* Member left listening */
 client.on(Events.GuildMemberRemove, async member => {
   /* WIP */
-  if (member.guild.id != "240492317679550465") return;
+  if (member.guild.id != process.env.LOGGING_SERVER) return;
 
   /* Kick detection */
   const fetchedLogs = await member.guild.fetchAuditLogs({
@@ -47,15 +47,15 @@ client.on(Events.GuildMemberRemove, async member => {
   const kickLog = fetchedLogs.entries.first();
 
   if (!kickLog) {
-    member.guild.channels.cache.get("497477596091514882")
+    member.guild.channels.cache.get(process.env.LOGGING_CHANNEL)
       .send(`${bold(member.user.tag)} (ID: ${member.id}) just left from server.`);
   } else {
     const { executor, target } = kickLog;
     if (target.id === member.id) {        
-      member.guild.channels.cache.get("497477596091514882")
+      member.guild.channels.cache.get(process.env.LOGGING_CHANNEL)
         .send(`${bold(member.user.tag)} (ID: ${member.id}) has been kicked from server by ${bold(executor.tag)} (ID: ${executor.id}). ${kickLog.reason ? `Reason: ${codeBlock(kickLog.reason)}` : ''}`);
     } else {
-      member.guild.channels.cache.get("497477596091514882")
+      member.guild.channels.cache.get(process.env.LOGGING_CHANNEL)
         .send(`${bold(member.user.tag)} left the server, audit log fetch was inconclusive.`);
     }
   }
@@ -64,7 +64,7 @@ client.on(Events.GuildMemberRemove, async member => {
 /* Ban listening */
 client.on(Events.GuildBanAdd, async ban => {
   /* WIP */
-  if (ban.guild.id != "240492317679550465") return;
+  if (ban.guild.id != process.env.LOGGING_SERVER) return;
 
   const fetchedLogs = await ban.guild.fetchAuditLogs({
     limit: 1,
@@ -73,14 +73,14 @@ client.on(Events.GuildBanAdd, async ban => {
 
   const banLog = fetchedLogs.entries.first();
 
-  ban.guild.channels.cache.get("497477596091514882")
+  ban.guild.channels.cache.get(process.env.LOGGING_CHANNEL)
     .send(`${bold(ban.user.tag)} (ID: ${ban.user.id}) has been banned by ${bold(banLog.executor.tag)} (ID: ${banLog.executor.id}). ${banLog.reason ? `Reason: ${codeBlock(banLog.reason)}` : ''}`)
 });
 
 /* Pardon listening */
 client.on(Events.GuildBanRemove, async ban => {
   /* WIP */
-  if (ban.guild.id != "240492317679550465") return;
+  if (ban.guild.id != process.env.LOGGING_SERVER) return;
 
   const fetchedLogs = await ban.guild.fetchAuditLogs({
     limit: 1,
@@ -89,6 +89,6 @@ client.on(Events.GuildBanRemove, async ban => {
 
   const banLog = fetchedLogs.entries.first();
 
-  ban.guild.channels.cache.get("497477596091514882")
+  ban.guild.channels.cache.get(process.env.LOGGING_CHANNEL)
     .send(`${bold(ban.user.tag)} (ID: ${ban.user.id}) has been pardoned (unbanned) by ${bold(banLog.executor.tag)} (ID: ${banLog.executor.id}).`)
 });
