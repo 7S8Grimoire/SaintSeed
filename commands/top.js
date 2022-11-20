ea3f4917caa969d614879e1363ec4e619d1e220f
SaintMiner <dinden290@gmail.com>
2022-11-20 14:23:39 +0200

    Top Connected

--- commands/top.js
+++ commands/top.js
@@ -3,6 +3,7 @@ const { EmbedBuilder } = require("discord.js");
 const i18next = require("i18next");
 const { profiles } = require("../modules/api");
 const moment = require("moment");
+const { paginationEmbed } = require("../modules/helpers");
 
 module.exports = {
   categories: ["command_spam", "roulette"],
@@ -17,6 +18,9 @@ module.exports = {
     )
     .addSubcommand((subcommand) =>
       subcommand.setName("voicepoints").setDescription("voicepoints top")
+    )
+    .addSubcommand((subCommand) => 
+      subCommand.setName('connected').setDescription("connected to server top")
     ),
   async execute(interaction) {
     const subCommand = interaction.options.getSubcommand();
@@ -58,24 +62,41 @@ module.exports = {
             let seconds = (`0` + (totalSeconds % 60)).slice(-2);
             
             let timeString = i18next.t('time.format', {days, hours, minutes, seconds});
-            topEmbed.addFields({ name: `#${ place++ } ${ member.displayName  }`, value: i18next.t('top.voiceTimeRow', {timeString}) });
+            topEmbed.addFields({ name: `#${ place++ } ${ member.displayName  }`, value: i18next.t('top.voiceTimeRow', { timeString }) });
         }
         return place > 5;
       });
     }
 
-    if (subCommand == "voicepoints") {
-      data = await profiles.pointsTop(interaction.guild.id)
-      topEmbed.setTitle(i18next.t("top.voicepoints"));
-      let place = 1;
-      data.some((profile) => {
-        let member = interaction.guild.members.cache.get(profile.user_id);
-        if (member) {
-          topEmbed.addFields({ name: `#${ place++ } ${member.displayName}`,  value: i18next.t('top.voicepointsRow', { voicepoints: profile.voicepoints })});
-        }
-        return place > 5;
+    if (subCommand == "connected") {
+      const members = interaction.guild.members.cache.sort((a, b) => {
+        return a.joinedTimestamp - b.joinedTimestamp 
       });      
+      let place = 1;
+      let pageItemCount = 1;
+      let pages = [];
+      let connectedTopEmbed = new EmbedBuilder()
+        .setColor(process.env.EMBED_PRIMARY_COLOR)
+        .setTitle(i18next.t("top.connected"));
+      members.forEach((member) => {
+        connectedTopEmbed.addFields({ 
+          name: `#${ place++ } ${ member.displayName  }`, 
+          value: i18next.t('top.joinedAt', { joinedDate: moment(member.joinedTimestamp).format('YYYY-MM-DD HH:mm') })
+        });        
+        pageItemCount++;        
+        if (pageItemCount > 10 || place > members.size) {
+          pageItemCount = 1;
+          pages.push(connectedTopEmbed);
+          connectedTopEmbed = new EmbedBuilder()
+            .setColor(process.env.EMBED_PRIMARY_COLOR)
+            .setTitle(i18next.t("top.connected"));
+        }
+      });
+      
+      return paginationEmbed(interaction, pages);
     }
+
+
     interaction.reply({
       embeds: [topEmbed]
     });
